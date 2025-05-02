521d07906bee16047214192902374fcc35460e49
janainamesquita <janainammesquita@hotmail.com>
2025-05-02 11:55:26 -0300

    paginadelogin

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,28 @@
+import { Link, useNavigate } from "react-router-dom"
+import svg from "/public/login-animate.svg"
+
 export default function LoginPage(){
+    const navigate = useNavigate()
+
     return (
-     <p>Aqui é a página de Login</p>
+
+        <div className="w-full h-screen flex">
+            <div className="w-[40%] h-full flex items-center justify-center">
+                <Link to="/cadastro">Cadastrar</Link>
+                <button
+                    onClick={() => navigate("/cadastro")}
+                >Cadastrar</button>
+                <button
+                    onClick={() => window.location.href = "/cadastro"}
+                >
+                    Cadastrar
+                </button>
+            </div>
+            <div className="w-[60%] h-full bg-[#6366EE] flex items-center justify-center rounded-l-lg flex-col">
+                <img width={300} src={svg} alt="imagem-animada" />
+                <p className="text-white font-bold w-[300px] text-[20px] text-center">A melhor experiencia de login que 
+                você já teve na sua vida!</p>
+            </div>
+        </div>
     )
 }
